6ce04e10948203f238a12f2e85cc57b530bae1a8
Jose Hernandez <jhernandez.sld@e.email>
2021-02-24 19:04:00 -0600

    added changes

--- uanl_analysis.py
+++ uanl_analysis.py
@@ -6,7 +6,11 @@ from typing import Tuple, List
 def print_tabulate(df: pd.DataFrame):
     print(tabulate(df, headers=df.columns, tablefmt='orgtbl'))
 
-def analysis(file_name:str)->pd.DataFrame:
-    df = pd.read_csv(file_name)
-    return df
-print_tabulate(analysis("csv/uanl.csv"))
+def analysis(file_name:str)->None:
+    df_complete = pd.read_csv(file_name)
+    df_by_dep = df.groupby("dependencia")[["Sueldo Neto"]]
+    print_tabulate(df_by_dep.min().sort_values(by=["Sueldo Neto"], ascending=False))
+
+
+analysis("csv/uanl.csv")
+#print_tabulate(df.head(50))
