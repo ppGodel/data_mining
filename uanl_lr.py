eb0fdb75979e97cba08d45569893aba28e8085f4
Jose Hernandez <jhernandez.sld@e.email>
2021-03-27 10:14:50 -0600

    fixed error in list

--- uanl_lr.py
+++ uanl_lr.py
@@ -27,8 +27,8 @@ def linear_regression(df: pd.DataFrame, x:str, y: str)->None:
 
     coef = pd.read_html(model.summary().tables[1].as_html(),header=0,index_col=0)[0]['coef']
     df.plot(x=x,y=y, kind='scatter')
-    plt.plot(df[x],[pd.DataFrame.mean(df[y]) for _ in fixed_x, color='green')
-    plt.plot(df_by_sal[x],[ coef.values[1] * x + coef.values[0] for x in fixed_x, color='red')
+    plt.plot(df[x],[pd.DataFrame.mean(df[y]) for _ in fixed_x], color='green')
+    plt.plot(df_by_sal[x],[ coef.values[1] * x + coef.values[0] for x in fixed_x], color='red')
     plt.xticks(rotation=90)
     plt.savefig(f'img/lr_{y}_{x}.png')
     plt.close()
@@ -39,5 +39,6 @@ df = pd.read_csv("csv/typed_uanl.csv")
 df_by_sal = df.groupby(["Fecha"])[["Sueldo Neto"]].aggregate(pd.DataFrame.mean)
 df_by_sal.reset_index(inplace=True)
 df_by_sal.columns=["Fecha", "sueldo_mensual"]
+# df_by_sal["sueldo_mensual"] = df_by_sal["sueldo_mensual"]**10
 print_tabulate(df_by_sal.head(5))
 linear_regression(df_by_sal, "Fecha", "sueldo_mensual")
