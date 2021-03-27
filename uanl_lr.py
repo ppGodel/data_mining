2186db0046d52f60f40b20020d62ab1cfb17370b
Jose Hernandez <jhernandez.sld@e.email>
2021-03-27 09:52:57 -0600

    updated lr

--- uanl_lr.py
+++ uanl_lr.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 import numbers
+from sklearn.preprocessing import PolynomialFeatures
+
 
 def transform_variable(df: pd.DataFrame, x:str)->pd.Series:
     if isinstance(df[x][0], numbers.Number):
@@ -18,27 +20,24 @@ def transform_variable(df: pd.DataFrame, x:str)->pd.Series:
         return [i for i in range(0, len(df[x]))]
 
 
-
 def linear_regression(df: pd.DataFrame, x:str, y: str)->None:
     fixed_x = transform_variable(df, x)
-    print(fixed_x)
     model= sm.OLS(df[y],sm.add_constant(fixed_x)).fit()
     print(model.summary())
 
     coef = pd.read_html(model.summary().tables[1].as_html(),header=0,index_col=0)[0]['coef']
-    df_by_sal.plot(x=x,y=y, kind='scatter')
-    plt.plot(df_by_sal[x],[pd.DataFrame.mean(df_by_sal[y]) for _ in range(0, len(df_by_sal[x]))], color='green')
-    plt.plot(df_by_sal[x],[ coef.values[1] * x + coef.values[0] for x in range(0, len(df_by_sal[x]))], color='red')
+    df.plot(x=x,y=y, kind='scatter')
+    plt.plot(df[x],[pd.DataFrame.mean(df[y]) for _ in fixed_x, color='green')
+    plt.plot(df_by_sal[x],[ coef.values[1] * x + coef.values[0] for x in fixed_x, color='red')
     plt.xticks(rotation=90)
     plt.savefig(f'img/lr_{y}_{x}.png')
     plt.close()
 
-    #model = ols(formula='Sueldo_Mensual ~ Fecha', data=df_by_sal).fit()
 
 df = pd.read_csv("csv/typed_uanl.csv")
+#print_tabulate(df.head(50))
 df_by_sal = df.groupby(["Fecha"])[["Sueldo Neto"]].aggregate(pd.DataFrame.mean)
 df_by_sal.reset_index(inplace=True)
 df_by_sal.columns=["Fecha", "sueldo_mensual"]
 print_tabulate(df_by_sal.head(5))
-linear_regression(df, "Fecha", "sueldo_mensual")
-#print_tabulate(df.head(50))
+linear_regression(df_by_sal, "Fecha", "sueldo_mensual")
